56a5b1c44c8ef6397e908d1c4cdc91c7a481252c
BernardoRaposo <raposobernardo@gmail.com>
2024-08-07 21:03:18 +0100

    second commit

--- app/components/Editor.js
+++ app/components/Editor.js
@@ -40,7 +40,7 @@ ${content}`;
   };
 
   return (
-    <div className="container mx-auto px-4 py-6">
+    <div className="container mx-auto px-4">
       <div className="mb-5">
         <input
           type="text"
@@ -67,7 +67,7 @@ ${content}`;
           className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
       </div>
-      <div className="h-96 mb-5">
+      <div className="h-96 mb-20">
         <ReactQuill 
           value={content} 
           onChange={setContent}
