bc8c0e420975468c64d394dba307e55a42dc1415
BernardoRaposo <raposobernardo@gmail.com>
2024-08-09 04:39:55 +0100

    Added Twitter Card to Meta info

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
       <link rel="icon" href={metadata.icon} />
+      <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://writedown-bernardoraposos-projects.vercel.app/" />
+        <meta property="twitter:title" content="WriteDown" />
+        <meta property="twitter:description" content="Simple blog, great content" />
+        <meta property="twitter:image" content="/public/writedown.png" />
+      </head>
       <body className={inter.className}>{children}
         <Footer />
       </body>
